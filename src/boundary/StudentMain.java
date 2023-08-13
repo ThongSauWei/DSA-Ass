26d3486a39e5201155584f5ecc8c4b6abe05a988
Benjamin030810 <benjaminncw-wm21@student.tarc.edu.my>
2023-08-14 01:31:26 +0800

    ermmm

---
+++ src/boundary/StudentMain.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boundary;
+
+import control.StudentControl;
+import entity.Student;
+import adt.ListInterface;
+/**
+ *
+ * @author User
+ */
+public class StudentMain {
+    private StudentControl studentControl;
+    ListInterface<Student> studentList;
+}
