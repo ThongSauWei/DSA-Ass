8b428631b7b384c0346f904bfe22865686c31139
Benjamin030810 <benjaminncw-wm21@student.tarc.edu.my>
2023-08-15 00:21:07 +0800

    ermmm

--- src/boundary/StudentMain.java
+++ src/boundary/StudentMain.java
@@ -7,11 +7,32 @@ package boundary;
 import control.StudentControl;
 import entity.Student;
 import adt.ListInterface;
+import java.util.Scanner;
 /**
  *
  * @author User
  */
 public class StudentMain {
-    private StudentControl studentControl;
-    ListInterface<Student> studentList;
+    private Scanner scanner = new Scanner(System.in);
+    private StudentControl studentControl = new StudentControl();
+    private ListInterface<Student> studentList = studentControl.readFromFile();
+    
+    public StudentMain() {
+        
+    }
+    
+    private int displayMenu() {
+        int choice;
+        
+        System.out.println("Welcome to Student Management");
+        System.out.println("=============================");
+        System.out.println("1. Add New Student");
+        System.out.println("2. Search Student");
+        System.out.println("3. Update Student Information");
+        System.out.println("4. Remove Student");
+        System.out.println("=============================");
+        System.out.print("Please Choose Your Option : ");
+               
+        return scanner.nextInt();
+    }
 }
